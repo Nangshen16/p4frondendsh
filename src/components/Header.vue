abb748ac7381e460d6780b61075b01b0d4392a13
Rachel ShenHom <rachelshenhom@Rachels-Air.fios-router.home>
2020-09-21 20:15:58 -0400

    about updated

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,11 +10,11 @@
         <b-navbar-item href="#">
           <router-link to="/">Home</router-link>
         </b-navbar-item>
-<!--        <b-navbar-item href="#">-->
+<!--        <b-navbar-item href="https://res.cloudinary.com/dcrioc0sw/image/upload/v1600732621/Screen_Shot_2020-09-21_at_7.56.30_PM_wwyf0r.png">-->
 <!--          <router-link to="/Page2">About</router-link>-->
 <!--        </b-navbar-item>-->
         <b-navbar-dropdown label="Info">
-          <b-navbar-item href="#">
+          <b-navbar-item href="https://res.cloudinary.com/dcrioc0sw/image/upload/v1600733569/Screen_Shot_2020-09-21_at_8.12.37_PM_tshd28.png">
             About
           </b-navbar-item>
           <b-navbar-item href="https://www.linkedin.com/in/nang-shen-hom/">
